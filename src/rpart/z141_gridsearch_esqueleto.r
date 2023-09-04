7916523db853617c591382f8149d79f8f1a4e8f2
msbeckel <ms.beckel@gmail.com>
2023-09-04 20:37:14 -0300

    feature engineering

--- src/rpart/z141_gridsearch_esqueleto.r
+++ src/rpart/z141_gridsearch_esqueleto.r
@@ -9,7 +9,7 @@ require("data.table")
 require("rpart")
 require("parallel")
 options(bitmapType = "cairo")
-
+require(caret)
 PARAM <- list()
 # reemplazar por las propias semillas
 semillas <- c(594697, 594709, 594721, 594739, 594749)
@@ -35,9 +35,42 @@ particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, 
 }
 #------------------------------------------------------------------------------
 
-ArbolEstimarGanancia <- function(semilla, param_basicos) {
+ArbolEstimarGanancia <- function(semilla, param_basicos, preprocess = TRUE) {
   # particiono estratificadamente el dataset
   particionar(dataset, division = c(7, 3), agrupa = "clase_ternaria", seed = semilla)
+
+  if (preprocess) {
+    dtrain <- dataset[fold == 1]
+    dtest <- dataset[fold == 2]
+
+    # Near zero variance variables
+    target <- which(colnames(dtrain) == "clase_ternaria")
+    nzv <- nearZeroVar(dtrain, saveMetrics = TRUE)
+    nzv[target, "nzv"] <- FALSE # fuerzo a que la variable a predecir no se nzv
+    dtrain <- dtrain[, !nzv$nzv, with = FALSE]
+    dtest <- dtest[, !nzv$nzv, with = FALSE]
+
+    # Transform variables
+    target <- which(colnames(dtrain) == "clase_ternaria")
+    tmp <- as.data.frame(dtrain)
+    tData <- preProcess(tmp[, c(-1, -target)], c("center", "scale", "medianImpute", "pca"))
+    # dtrain
+    tmp[, c(-1, -target)] <- predict(tData, tmp[, c(-1, -target)])
+    dtrain <- setDT(tmp)
+    # dtest
+    tmp <- as.data.frame(dtest)
+    tmp[, c(-1, -target)] <- predict(tData, tmp[, c(-1, -target)])
+    dtest <- setDT(tmp)
+    rm(tmp)
+    gc()
+
+    # Final data.table
+    dtrain[, fold := 1]
+    dtest[, fold := 2]
+    dataset <- rbindlist(list(dtrain, dtest))
+    rm(dtrain, dtest)
+    gc()
+  }
 
   # genero el modelo
   # quiero predecir clase_ternaria a partir del resto
@@ -101,12 +134,13 @@ dataset <- fread("./data/competencia_01.csv")
 # trabajo solo con los datos con clase, es decir 202107
 dataset <- dataset[clase_ternaria != ""]
 
+
 # genero el archivo para Kaggle
 # creo la carpeta donde va el experimento
 # HT  representa  Hiperparameter Tuning
 dir.create("./exp/", showWarnings = FALSE)
 dir.create("./exp/HT2020/", showWarnings = FALSE)
-archivo_salida <- "./exp/HT2020/gridsearch.txt"
+archivo_salida <- "./exp/HT2020/gridsearch3.txt"
 
 # Escribo los titulos al archivo donde van a quedar los resultados
 # atencion que si ya existe el archivo, esta instruccion LO SOBREESCRIBE,
@@ -125,40 +159,40 @@ cat(
 
 # itero por los loops anidados para cada hiperparametro
 t0 <- Sys.time()
-for (cp in c(-1)){
-    for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
-      for (vmin_split in c(1000, 800, 600, 400, 200, 100, 50, 20, 10)) {
-        for (mb in c(c(1, as.integer(vmin_split / 2)))){
-          # notar como se agrega
+for (cp in c(-1)) {
+  for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
+    for (vmin_split in c(1000, 800, 600, 400, 200, 100, 50, 20, 10)) {
+      for (mb in c(c(1, as.integer(vmin_split / 4), as.integer(vmin_split / 2)))) {
+        # notar como se agrega
 
-          # vminsplit  minima cantidad de registros en un nodo para hacer el split
-          param_basicos <- list(
-            "cp" = cp, # complejidad minima
-            "minsplit" = vmin_split,
-            "minbucket" = mb, # minima cantidad de registros en una hoja
-            "maxdepth" = vmax_depth
-          ) # profundidad máxima del arbol
+        # vminsplit  minima cantidad de registros en un nodo para hacer el split
+        param_basicos <- list(
+          "cp" = cp, # complejidad minima
+          "minsplit" = vmin_split,
+          "minbucket" = mb, # minima cantidad de registros en una hoja
+          "maxdepth" = vmax_depth
+        ) # profundidad máxima del arbol
 
-          # Un solo llamado, con la semilla 17
-          ganancia_promedio <- ArbolesMontecarlo(semillas, param_basicos)
+        # Un solo llamado, con la semilla 17
+        ganancia_promedio <- ArbolesMontecarlo(semillas, param_basicos)
 
-          # escribo los resultados al archivo de salida
-          cat(
-            file = archivo_salida,
-            append = TRUE,
-            sep = "",
-            cp, "\t",
-            vmax_depth, "\t",
-            mb, "\t",
-            vmin_split, "\t",
-            ganancia_promedio, "\n"
-          )
-        }
+        # escribo los resultados al archivo de salida
+        cat(
+          file = archivo_salida,
+          append = TRUE,
+          sep = "",
+          cp, "\t",
+          vmax_depth, "\t",
+          mb, "\t",
+          vmin_split, "\t",
+          ganancia_promedio, "\n"
+        )
       }
+    }
   }
 }
 tiempo <- as.numeric(Sys.time() - t0)
 
-#Leo los resultados
-res = fread("/home/maxibeckel/maestria_datos/dmeyf/dmeyf2023/exp/HT2020/gridsearch.txt")
+# Leo los resultados
+res <- fread("/home/maxibeckel/maestria_datos/dmeyf/dmeyf2023/exp/HT2020/gridsearch.txt")
 View(res[order(-ganancia_promedio)])
